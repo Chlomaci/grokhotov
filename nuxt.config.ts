ad706ac53a6495d4e70226d0e7160e312e764e7a
Дарина <darinaeme@gmail.com>
2024-11-19 22:18:58 +0300

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default ({
   devtools: {enabled: true},
   modules: ['@pinia/nuxt'],
   app: {
-    baseURL: "/grokhotov/",
+    baseURL: "/",
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
@@ -14,7 +14,7 @@ export default ({
       ],
     },
     router: {
-      base: '/grokhotov/'
+      base: '/'
     },
     target: 'static',
   }
