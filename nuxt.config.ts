51e053b960ec8507a348e3e25cb2168b3f0f716e
Дарина <darinaeme@gmail.com>
2024-11-19 19:03:55 +0300

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default ({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
   app: {
     baseURL: "/yourrepositoryname/"
-  }
+  },
+  target: 'static'
 })
